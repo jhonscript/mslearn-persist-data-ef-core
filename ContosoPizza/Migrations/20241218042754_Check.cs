4a3186e8e80135b8ea7274c0cb33a9158ff7c3c5
Jhon Guevara <jsgv93@gmail.com>
2024-12-17 23:33:29 -0500

    Scaffold the promotions context and coupon model

---
+++ ContosoPizza/Migrations/20241218042754_Check.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    /// <inheritdoc />
+    public partial class Check : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegan",
+                table: "Sauces",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVegan",
+                table: "Sauces");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+        }
+    }
+}
